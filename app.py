0d22112f008cdd7d373b1bba131b289ccbf2b972
Fatemeh Taher <Hyuuga_neji1375@yahoo.com>
2025-02-01 12:56:05 +0330

    add alibaba font

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # Configure Babel
 app.config['LANGUAGES'] = ['en', 'fa']  # English and Persian
-app.config['BABEL_DEFAULT_LOCALE'] = 'fa'
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
 app.config['BABEL_TRANSLATION_DIRECTORIES'] = 'translations'
 
 babel = Babel(app)
@@ -14,12 +14,12 @@ babel = Babel(app)
 def get_locale():
     return request.args.get('lang', app.config['BABEL_DEFAULT_LOCALE'])
 
-# Initialize Babel
-babel.init_app(app, locale_selector=get_locale)
-
 # Function to determine text direction
 def get_direction(lang):
     return 'rtl' if lang == 'fa' else 'ltr'
+
+# Initialize Babel
+babel.init_app(app, locale_selector=get_locale)
 
 @app.route('/')
 def home():
@@ -27,11 +27,28 @@ def home():
     direction = get_direction(lang)  # Determine the text direction
     return render_template('index.html', lang=lang, direction=direction)
 
-@app.route('/blog')
-def blog():
-    return render_template('blog.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-if __name__ == "__main__":
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/product')
+def product():
+    return render_template('product.html')
+
+@app.route('/service')
+def service():
+    return render_template('service.html')
+
+if __name__ == '__main__':
     with app.test_request_context():
         print(url_for('static', filename='en'))  # Should output "/?lang=en"
     app.run(debug=True)
+
